8f3957434efca0b0ace28d6f178c5d2e3f0b88f6
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-03-25 16:32:30 -0700

    main.ts: Fix formatting

--- main.ts
+++ main.ts
@@ -1,8 +1,5 @@
 import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.181.0/http/file_server.ts";
 
-serve((r: Request) => {
-  return serveDir(r, {
-      fsRoot: "site/",
-    });
-});
+serve((r: Request) => serveDir(r, { fsRoot: "site/" });
+      
