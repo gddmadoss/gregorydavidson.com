f67f28d2a354800c8abf230add47eff57f525d34
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-03-27 15:51:18 -0800

    WIP

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Source code of https://gregorydavidson.com/ as of January 11th, 2019.
 
 ## Information
 
-This site is a hobby project of [**@gddmad**](https://github.com/gddmad)'s. The site was updated manually via FTP connection to the hosting provider via [Total Commander](https://www.ghisler.com/).
+This site is a hobby project of [**@gddmad**](https://github.com/gddmad)'s. The
+site was updated manually via FTP connection to the hosting provider via
+[Total Commander](https://www.ghisler.com/).
 
 The hosting provider of https://gregorydavidson.com/ is unknown.
 
--- main.ts
+++ main.ts
@@ -1,5 +1,35 @@
-import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
-import { serveDir } from "https://deno.land/std@0.181.0/http/file_server.ts";
+import { serve, serveDir } from "./deps.ts";
 
-serve((r: Request) => serveDir(r, { fsRoot: "site/" }));
-      
+enum Version {
+  Y2001 = "/2001",
+  Y2019 = "/2019",
+}
+
+const VERSION_PATHS = {
+  [Version.Y2001]: "2001/",
+  [Version.Y2019]: "2019/",
+};
+
+const DEFAULT_VERSION = Version.Y2019;
+
+serve((r: Request) => {
+  const u = new URL(r.url);
+  // TODO: Fix 404 on /2001/
+  if (u.pathname.startsWith(Version.Y2001)) {
+    return serveVersion(r, Version.Y2001);
+  }
+
+  // TODO: Fix 404 on /2019/
+  if (u.pathname.startsWith(Version.Y2019)) {
+    return serveVersion(r, Version.Y2019);
+  }
+
+  return Response.redirect(new URL(DEFAULT_VERSION, u).href, 301);
+});
+
+function serveVersion(r: Request, v: Version) {
+  return serveDir(r, {
+    fsRoot: VERSION_PATHS[v],
+    urlRoot: v,
+  });
+}
